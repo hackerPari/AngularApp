3fccffa291f8fa19541f51dbf6e4667470bf2c3c
parikshit <parikshit@invideo.io>
2019-01-26 16:43:17 +0530

    added comments and removed app-routing and pagenotfound component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ShowDataComponent } from './views/show-data/show-data.component';
-import { PagenotfoundComponent } from './views/pagenotfound/pagenotfound.component';
 
-import { AppRoutingModule } from './views/app-routing/app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { ChapterFilterPipe } from './pipes/chapter-filter.pipe';
 
@@ -14,13 +12,11 @@ import { ChapterFilterPipe } from './pipes/chapter-filter.pipe';
   declarations: [
     AppComponent,
     ShowDataComponent,
-    PagenotfoundComponent,
     ChapterFilterPipe
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
--- src/app/pipes/chapter-filter.pipe.ts
+++ src/app/pipes/chapter-filter.pipe.ts
@@ -8,15 +8,23 @@ export class ChapterFilterPipe implements PipeTransform {
   transform(chapters: any, filter?: any): any {
     console.log(chapters, filter);
 
+    // only filter if search term is not empty string
     if (filter.length > 0) {
+
+      // contains filtered chapters which will be returned
       const filteredChapters = [];
 
       chapters.forEach(chapter => {
+
+        // search for the chapter name which matches with the search term
         if (chapter.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0) {
           filteredChapters.push(chapter);
-        } else {
+        } else { // try matching the topics of the chapter if chapter name doesn't match
 
+          // contains filtered topics which we need to show
           const filteredTopics = [];
+
+          // make a deep copy of the chapter object
           const filteredChapter = JSON.parse(JSON.stringify(chapter));
 
           chapter.topics.forEach(topic => {
@@ -25,6 +33,8 @@ export class ChapterFilterPipe implements PipeTransform {
             }
           });
 
+
+          // only if at least one topic matches the search term, we add the chapter to the filteredChapters
           if (filteredTopics.length > 0) {
             filteredChapter.topics = filteredTopics;
             filteredChapters.push(filteredChapter);
